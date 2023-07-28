7dbe12ecb214f60b4ec4197b4e43e51021ffe51e
KDrip34 <124319502+KDrip34@users.noreply.github.com>
2023-07-28 16:12:00 -0700

    Create dripautomotives.py

---
+++ dripautomotives.py
@@ -0,0 +1,4 @@
+car = input("Hello welcome to Drip Automotives what type of car are you looking for today: ")
+print("Right on thats a good choice, " + car  + " are very expensive but over here we like to give out amazing prices,!")
+colorway = input("What color do you think best complements you car:")
+print("So your leaning more towards , " + colorway + " risky but diffrent let me see what I can find for you!")
